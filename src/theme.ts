21ba58bd06b6ec5dd63c6a65ad3533575a68e64a
dokkalemon <dottori.roberto@gmail.com>
2023-03-18 20:52:46 +0100

    add theme, Logo and font

---
+++ src/theme.ts
@@ -0,0 +1,71 @@
+import {
+  createTheme,
+  responsiveFontSizes,
+} from "@mui/material";
+
+const theme = responsiveFontSizes(
+  createTheme({
+    palette: {
+      primary: {
+        light: "#EF3E44",
+        main: "#E01E23",
+        dark: "#A7040A",
+      },
+      secondary: {
+        light: "#FCC26C",
+        main: "#ff9800",
+        dark: "#ff3000",
+      },
+      grey: {
+        100: "#FFFFFF",
+        200: "#eeeeee",
+        300: "#DFE3E8",
+        400: "#C4CDD5",
+        500: "#919EAB",
+        600: "#637381",
+        700: "#454F5B",
+        800: "#121212",
+        900: "#000000",
+      },
+    },
+    typography: {
+      fontFamily: "Roboto, sans-serif",
+      fontWeightLight: 300,
+      fontWeightRegular: 400,
+      fontWeightMedium: 500,
+      fontWeightBold: 700,
+      h1: {
+        fontSize: "67.7px",
+        fontWeight: 400,
+      },
+      h5: {
+        fontSize: "24px",
+        fontWeight: 400,
+      },
+      body1: {
+        fontSize: "16px",
+        fontWeight: 400,
+      },
+      body2: {
+        fontSize: "18px",
+        fontWeight: 500,
+      },
+      caption: {
+        fontSize: "14px",
+        fontWeight: 400,
+      },
+      subtitle1: {
+        fontSize: "25px",
+        fontWeight: 500,
+      },
+      subtitle2: {
+        fontSize: "25px",
+        fontWeight: 500,
+      },
+    },
+    breakpoints: {},
+    components: {},
+  })
+);
+
+export { theme };
